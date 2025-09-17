db037dda0714b2cece6c4d55f48658ac7f8622d3
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-17 23:21:49 +0530

    Create Home.js

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { products } from "../../data/products";
+import ProductCard from "../../components/ProductCard/ProductCard";
+
+const Home = () => {
+  return (
+    <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
+
+export default Home;
