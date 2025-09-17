6a192a0de29afd7fd90453c66ded2c3fc0ee185d
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-17 23:42:29 +0530

    Create api.js

---
+++ src/utils/api.js
@@ -0,0 +1,9 @@
+import { products } from "../data/products";
+
+export const fetchProducts = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(products);
+    }, 500);
+  });
+};
