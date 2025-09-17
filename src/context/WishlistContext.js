8a6bbd0ca1751e96ebfe91d931d915a7fe339f4b
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-17 23:17:57 +0530

    Create WishlistContext.js

---
+++ src/context/WishlistContext.js
@@ -0,0 +1,25 @@
+import React, { createContext, useContext, useState } from "react";
+
+const WishlistContext = createContext();
+
+export const useWishlist = () => useContext(WishlistContext);
+
+export const WishlistProvider = ({ children }) => {
+  const [wishlist, setWishlist] = useState([]);
+
+  const addToWishlist = (product) => {
+    setWishlist((prev) => [...prev, product]);
+  };
+
+  const removeFromWishlist = (id) => {
+    setWishlist((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  return (
+    <WishlistContext.Provider
+      value={{ wishlist, addToWishlist, removeFromWishlist }}
+    >
+      {children}
+    </WishlistContext.Provider>
+  );
+};
