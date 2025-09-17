b4ce8818cae28740a9a5b85994bf5bb65da5627a
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-18 00:42:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 import { WishlistProvider } from "./context/WishlistContext";
 import { ThemeProvider } from "./context/ThemeContext";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Checkout from "./pages/Checkout/Checkout";
 
 function App() {
   return (
     <ThemeProvider>
       <CartProvider>
         <WishlistProvider>
-          <Header />
-          <main className="min-h-[80vh]">
-            <Home />
-          </main>
-          <Footer />
+          <Router>
+            <Header />
+            <main className="min-h-[80vh]">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/checkout" element={<Checkout />} />
+              </Routes>
+            </main>
+            <Footer />
+          </Router>
         </WishlistProvider>
       </CartProvider>
     </ThemeProvider>
