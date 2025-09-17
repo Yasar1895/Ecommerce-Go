cb1f052c13e16660ad1225a9a7542bedfdef1870
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-18 00:21:43 +0530

    Update WishlistContext.js

--- src/context/WishlistContext.js
+++ src/context/WishlistContext.js
@@ -1,20 +1,28 @@
-import React, { createContext } from "react";
+import React, { createContext, useContext } from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
 
 export const WishlistContext = createContext();
+
+export const useWishlist = () => {
+  const ctx = useContext(WishlistContext);
+  if (!ctx) {
+    throw new Error("useWishlist must be used within a WishlistProvider");
+  }
+  return ctx;
+};
 
 export const WishlistProvider = ({ children }) => {
   const [wishlist, setWishlist] = useLocalStorage("wishlist", []);
 
   const addToWishlist = (product) => {
     setWishlist((prev) => {
-      if (prev.find((item) => item.id === product.id)) return prev;
+      if (prev.find((p) => p.id === product.id)) return prev;
       return [...prev, product];
     });
   };
 
   const removeFromWishlist = (id) => {
-    setWishlist((prev) => prev.filter((item) => item.id !== id));
+    setWishlist((prev) => prev.filter((p) => p.id !== id));
   };
 
   return (
